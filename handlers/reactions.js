bee4458d0de87594b7697ccaa05f2b1a8fd995af
jay1934 <73715013+jay1934@users.noreply.github.com>
2021-02-17 14:05:35 -0500

    Update reactions.js

--- handlers/reactions.js
+++ handlers/reactions.js
@@ -1,4 +1,12 @@
-exports.init = (client) =>
-  Object.entries(client.messages()).forEach(async ([id, { channel }]) =>
-    client.channels.cache.get(channel).messages.fetch(id)
+exports.init = (client) => {
+  const messages = client.messages();
+  Object.entries(messages).forEach(async ([id, { channel }]) =>
+    client.channels.cache.get(channel).messages.fetch(id).catch(() => {
+      delete messages[id];
+      require('fs').writeFielSync(
+        './data/reactionDatabase.json',
+        JSON.stringify(messages, '', 2)
+      );
+    })
   );
+}
